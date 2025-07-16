02fb2a5059c3971b1479c71654e81b2a02146afd
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-07-15 18:53:15 -0700

    Finish initializing SwerveDriveOdometry object to be able to use

---
+++ src/main/java/frc/robot/odometry/OdometryState.java
@@ -0,0 +1,5 @@
+package frc.robot.odometry;
+
+public enum OdometryState {
+  DEFAULT_STATE;
+}
--- src/main/java/frc/robot/odometry/OdometrySubsystem.java
+++ src/main/java/frc/robot/odometry/OdometrySubsystem.java
@@ -1,26 +1,44 @@
 package frc.robot.odometry;
 
+import static edu.wpi.first.units.Units.Inches;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import frc.robot.imu.ImuSubsystem;
+import frc.robot.swerve.SwerveSubsystem;
+import frc.robot.util.scheduling.SubsystemPriority;
+import frc.robot.util.state_machines.StateMachine;
 
-public class OdometrySubsystem {
-  private static final Translation2d[] modulePositions = {
-    new Translation2d(12, 12), // front-left
-    new Translation2d(12, -12), // front-right
-    new Translation2d(-12, 12), // back-left
-    new Translation2d(-12, -12) // back-right
+public class OdometrySubsystem extends StateMachine<OdometryState> {
+  private static final Translation2d[] robotRelativeModulePositions = {
+    new Translation2d(Inches.of(12), Inches.of(12)), // front-left
+    new Translation2d(Inches.of(12), Inches.of(-12)),          // front-right
+    new Translation2d(Inches.of(-12), Inches.of(12)),          // back-left
+    new Translation2d(Inches.of(-12), Inches.of(-12))                    // back-right
   };
-  private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(modulePositions);
-  private final Rotation2d gyroAngle = new Rotation2d();
+  private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(robotRelativeModulePositions);
+  private final ImuSubsystem imu;
+  private final SwerveSubsystem swerve;
+  private final SwerveDriveOdometry wpiOdometry;
 
-  // private SwerveDriveOdometry wpiOdometry = new SwerveDriveOdometry(kinematics, gyroAngle, new
-  // SwerveModulePosition[]);
+  public OdometrySubsystem(ImuSubsystem imu, SwerveSubsystem swerve) {
+    super(SubsystemPriority.ODOMETRY, OdometryState.DEFAULT_STATE);
+    this.imu = imu;
+    this.swerve = swerve;
 
-  public OdometrySubsystem() {}
+    final Rotation2d initialHeading = Rotation2d.fromDegrees(imu.getRobotHeading());
+    final SwerveModulePosition[] fieldRelativeModulePositions = swerve.drivetrain.getState().ModulePositions;
+    this.wpiOdometry = new SwerveDriveOdometry(kinematics, initialHeading, fieldRelativeModulePositions);
+  }
 
-  public void resetPosition(Rotation2d gyroAngle, SwerveModulePosition[] modulePositions) {
-    // wpiOdometry.resetPosition(gyroAngle, modulePositions);
+  public void resetPosition() {
+
+  }
+
+  public void update() {
+
   }
 }
