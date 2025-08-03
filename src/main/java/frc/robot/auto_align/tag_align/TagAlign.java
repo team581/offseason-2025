937a9d641f747a19c9f82ff8507a450dfc96a0c8
Simon <87281874+simonp17@users.noreply.github.com>
2025-08-02 19:51:20 -0700

    Remove unused variable

--- src/main/java/frc/robot/auto_align/tag_align/TagAlign.java
+++ src/main/java/frc/robot/auto_align/tag_align/TagAlign.java
@@ -82,7 +82,6 @@ public class TagAlign {
   private double rawControllerYValue = 0.0;
   private double lastPipeSwitchTimestamp = 0.0;
   private boolean aligned = false;
-  private static final double DISTANCE_TO_CENTERED = Double.MAX_VALUE;
   private OptionalDouble coralL1Offset = OptionalDouble.empty();
 
   private final LinearFilter l1AdjustmentFilter = LinearFilter.movingAverage(7);
