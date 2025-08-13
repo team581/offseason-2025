fbc19e676724b4808be1b11e56bbab4db29f6d75
Simon <87281874+simonp17@users.noreply.github.com>
2025-08-13 07:40:50 -0700

    Change climber lineup angle

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -4,7 +4,7 @@ public enum ClimberState {
   STOPPED(98.0, 0.0),
 
   LINEUP_FORWARD(0, 4.0),
-  LINEUP_BACKWARD(10, 2.0),
+  LINEUP_BACKWARD(0, 2.0),
   HANGING(99.0, 12.0);
 
   public final double angle;
